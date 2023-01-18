6c595aad7ee0aa14a765ec8ea42ccae83172f3ab
Nishith P <nishithp11@gmail.com>
2023-01-18 18:20:21 +0000

    Added Comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Importing the required dependencies
 const express = require('express');
 const app = express();
 const asciify = require('asciify-image');
@@ -5,17 +6,20 @@ const fs = require('fs');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 require('dotenv').config();
 
+// Creating an express server at port: 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 })
 
+// Base path "/" initialized to send application status
 app.get("/", (req, res) => {
   res.json({
     message: "Hello World"
   })
 })
 
+// API endpoint to convert a given image url to ASCII Art and return it in the form of a txt file
 app.get("/image-to-ascii", (req, res) => {
   if (!req.query.image) {
     res.json({
@@ -37,13 +41,16 @@ app.get("/image-to-ascii", (req, res) => {
       } else {
         fs.writeFileSync('out.txt', asciified);
         res.sendFile(__dirname + "/out.txt", () => {
-          fs.unlinkSync("out.txt")
+          fs.writeFileSync("out.txt", "", (err) => {
+            if(err) console.log(err);
+          })
         });
       }
     });
   }
 })
 
+// Module to connect to the Spotify API and retrieve data related to the playlistID provided as an argument
 async function getPlaylist(playlistID) {
   let playlist = [];
   let response = await fetch(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, {
@@ -60,7 +67,7 @@ async function getPlaylist(playlistID) {
     response.items.forEach(track => {
       playlist.push({
         name: track["track"].album.name,
-        image: track["track"].album.images[0].url
+        image: track["track"].album.images[1].url
       })
     })
   } catch (err) {
@@ -73,6 +80,7 @@ async function getPlaylist(playlistID) {
   return playlist;
 }
 
+// API endpoint to use the getPlaylist() module and send the returned data as the response
 app.get("/spotify/playlist/:playlistID", async (req, res) => {
   if (!req.params.playlistID) {
     res.json({
@@ -95,6 +103,11 @@ app.get("/spotify/playlist/:playlistID", async (req, res) => {
   }
 })
 
+/* API endpoint to call the getPlaylist() module twice - once for the main playlist and second time for the dummy playlist
+   Use the returned data to select 10 random albums from the main playlist along, get their names and image urls, convert 
+   the images to ASCII art and populate the options list with the correct answer and 3 other wrong options and send the 
+   compiled game.txt as part of the response
+*/
 app.get("/spotify/playlist/:playlistID/game.txt", async (req, res) => {
   if (!req.params.playlistID) {
     res.json({
@@ -107,15 +120,15 @@ app.get("/spotify/playlist/:playlistID/game.txt", async (req, res) => {
       let dummyValues = await getPlaylist("2w3lbcL6WV04kZHHggmB98");
       let question_set = new Set(), answer_set = new Set();
 
-      while (question_set.size < 10) {
+      while (question_set.size != 10) {
         question_set.add(playlist[Math.floor(Math.random() * playlist.length)]);
       }
 
       question_set.forEach(album => {
         asciify(album.image, {
           fit: 'box',
-          width: 200,
-          height: 100,
+          width: 64,
+          height: 64,
           color: false
         }, function(err, asciified) {
           if (err) {
@@ -128,13 +141,15 @@ app.get("/spotify/playlist/:playlistID/game.txt", async (req, res) => {
               answer_set.add(dummyValues[Math.floor(Math.random() * dummyValues.length)].name);
             }
             let options = randomOptionString(album.name, Array.from(answer_set));
-            fs.appendFileSync("game.txt", "\n-- BEGIN QUESTION --\n" + asciified + "\n\t\n" + options.options + "\n\t\n-- END QUESTION --\n" + options.key_index);
+            fs.appendFileSync("game.txt", "-- BEGIN QUESTION --\n" + asciified + "\n\t\n" + options.options + "\n\t\n-- END QUESTION --\n" + options.key_index + "\n");
           }
         });
       })
 
       res.sendFile(__dirname + "/game.txt", () => {
-        fs.unlinkSync("game.txt");
+        fs.writeFileSync("game.txt", "", (err) => {
+            if(err) console.log(err);
+          });
       })
     } catch (err) {
       if (err) res.json({
@@ -145,6 +160,7 @@ app.get("/spotify/playlist/:playlistID/game.txt", async (req, res) => {
   }
 })
 
+// Module to format the dummy values and the answer key to the required String format
 function randomOptionString(key, dummyValues) {
   let options = [0, 0, 0, 0];
   let key_index = Math.floor(Math.random() * 4);
@@ -159,3 +175,7 @@ function randomOptionString(key, dummyValues) {
 
   return { key_index: key_index + 1, options: options.join("\n") }
 }
+
+// Made with ❤️ by Nishith P
+// PSAT Project API Wrapper
+// 2023
